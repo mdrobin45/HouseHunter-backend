84ebb4496348986098d60fede9db2c08c010416e
Md Robin Rana <developer.smrobin45@gmail.com>
2024-01-23 20:37:30 +0600

    Login reg added

--- src/controllers/authentication/registration.js
+++ src/controllers/authentication/registration.js
@@ -3,19 +3,26 @@ const UserModel = require("../../schemas/userSchema");
 
 const userRegistration = async (req, res) => {
    try {
-      const { name, email, password } = req.body;
+      const { name, email, password, role, phone } = req.body;
 
       // Hash the password
       const hashedPassword = await bcrypt.hash(password, 10);
 
       // Create new user
-      const user = new UserModel({ name, email, password: hashedPassword });
+      const user = new UserModel({
+         name,
+         email,
+         role,
+         phone,
+         password: hashedPassword,
+      });
 
       // Save user to database
       await user.save();
 
       res.status(201).json({ message: "User registered successfully" });
-   } catch {
+   } catch (err) {
+      console.log(err);
       res.status(500).json({ error: "Internal server error" });
    }
 };
--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -5,14 +5,8 @@ const userSchema = mongoose.Schema(
       name: { type: String, required: true },
       email: { type: String, required: true },
       password: { type: String, required: true },
-      enrolled: [
-         {
-            status: { type: String, default: "In Progress" },
-            complete: { type: Number, default: 10 },
-            dueDate: { type: String },
-            course: { type: mongoose.Types.ObjectId, ref: "Courses" },
-         },
-      ],
+      phone: { type: Number, required: true },
+      role: { type: String, required: true },
    },
    { timeStamp: true }
 );
